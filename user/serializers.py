70666a3ab53849626cd42e7de4eed5b21ac3250f
Andriy_Shumskyy <andshumskyy@gmail.com>
2024-01-30 12:05:55 +0200

    freezed requirements, created view, url, model and serializer for user(abstractuser) in user app

--- user/serializers.py
+++ user/serializers.py
@@ -7,3 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ("id", "username", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
